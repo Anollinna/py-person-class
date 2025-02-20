d2ebff33054f6c911ae07ec134dfe1b962780e1c
Khrystyna Sen <anollina@ukr.net>
2025-02-20 23:19:37 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -21,11 +21,13 @@ def create_person_list(people: list) -> list:
     for human in people:
         current_person = Person.people.get(human["name"])
         current_wife = human.get("wife")
-        if current_wife:
+        if current_wife and current_wife in Person.people:
             current_person.wife = Person.people[current_wife]
 
+
         current_husband = human.get("husband")
-        if current_husband:
+        if current_husband and current_husband in Person.people:
             current_person.husband = Person.people[current_husband]
+
 
     return person_list
